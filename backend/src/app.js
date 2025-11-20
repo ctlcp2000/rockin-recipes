5e5bd3a14842656c0fbfec1464c7e3c564946f45
ctlcp2000 <charlieadamsyes@gmail.com>
2025-11-20 22:35:05 +0000

    feat: add socket.io

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,11 @@ import { recipeRoutes } from './routes/recipes.js'
 import { userRoutes } from './routes/users.js'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import { createServer } from 'node:http'
+import { Server } from 'socket.io'
+import { handleSocket } from './socket.js'
+
+
 
 const app = express()
 app.use(cors())
@@ -14,6 +19,22 @@ userRoutes(app)
 app.get('/', (req, res) => {
   res.send('Hello from Express!')
 })
+const server = createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+})
+handleSocket(io)
+// io.on('connection', (socket) => {
+//   console.log('user connected:', socket.id)
+//   socket.on('disconnect', () => {
+//     console.log('user disconnected:', socket.id)
+//   })
+// })
+export { server as app }
+// export { app }
+
 
 app.post('/api/v1/recipes', async (req, res) => {
   try {
@@ -55,4 +76,3 @@ app.delete('/api/v1/recipes/:id', async (req, res) => {
     return res.status(500).end()
   }
 })
-export { app }
---
+++ backend/src/socket.js
@@ -0,0 +1,12 @@
+export function handleSocket(io) {
+  io.on('connection', (socket) => {
+    console.log('user connected:', socket.id)
+    socket.on('disconnect', () => {
+      console.log('user disconnected:', socket.id)
+    })
+    socket.on('chat.message', (message) => {
+      console.log(`${socket.id}: ${message}`)
+      socket.broadcast.emit('chat.message', { username: socket.id, message })
+    })
+  })
+}
